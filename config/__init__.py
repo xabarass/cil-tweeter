fb58848f1445650e949c22706f3985c6e3dac3f6
Lukas Drescher <lukasd@student.ethz.ch>
2017-06-13 15:31:34 +0200

    Fixed config, extended gitignore

--- config/__init__.py
+++ config/__init__.py
@@ -3,11 +3,11 @@ import getpass
 user_name = getpass.getuser()
 
 # Configuration data
-if user_name in set("milan","pmilan"):
+if user_name in {"milan","pmilan"}:
     from .milan_config import *
-elif user_name in set("franzm"):
+elif user_name in {"franzm"}:
     from .michael_config import *
-elif user_name in set("allandre"):
+elif user_name in {"allandre"}:
     from .andreas_config import *
-elif user_name in set("lukas","lukasd"):
+elif user_name in {"lukas","lukasd"}:
     from .lukas_config import *
