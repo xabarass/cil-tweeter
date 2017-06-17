dd384ea84afa0f4a5e34e88de4776e0325974414
Lukas <lukasd@student.ethz.ch>
2017-06-17 00:01:34 +0000

    Automated commit of run results

--- run_and_commit.sh
+++ run_and_commit.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 echo "Running main and committing & pushing results..."
-python3 main.py
+#python3 main.py
 git add results/*
-git commit -m "Automated commit of results"
+if [[ $# -ne 1 ]]; then 
+    git commit -m "Automated commit of run results"
+else
+    git commit -m "Automated commit of run results - $1"
+fi
 git push origin $(git symbolic-ref --short -q HEAD)
 echo "...automated commit & push finished!"
