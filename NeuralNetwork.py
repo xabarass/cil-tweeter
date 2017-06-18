2110eeff9746026fcd07da24e3253f1afe694aa1
Milan Pandurov <milanpandurov@gmail.com>
2017-06-18 13:02:15 +0200

    Removed all conv bullshit, left just lstm!

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -97,16 +97,16 @@ class Network:
 
         model = Sequential()
         model.add(embedding_layer)
-        model.add(Convolution1D(350,
-                                3,
-                                padding='valid',
-                                activation='relu',
-                                strides=1))
-        model.add(Convolution1D(350,
-                                7,
-                                padding='valid',
-                                activation='relu',
-                                strides=1))
+        # model.add(Convolution1D(350,
+        #                         4,
+        #                         padding='valid',
+        #                         activation='relu',
+        #                         strides=1))
+        # model.add(Convolution1D(350,
+        #                         7,
+        #                         padding='valid',
+        #                         activation='relu',
+        #                         strides=1))
         model.add(LSTM(150))
         model.add(Dropout(0.5))
         model.add(Dense(1, activation='sigmoid'))
