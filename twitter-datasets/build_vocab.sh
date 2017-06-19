6e3a56fd6228d8dffba168263d8cacb0b1149c59
Lukas <lukasd@student.ethz.ch>
2017-06-19 23:00:08 +0000

    Vocab building fix

--- twitter-datasets/build_vocab.sh
+++ twitter-datasets/build_vocab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Generating short dict"
-cat train_pos.txt train_neg.txt test_data.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > vocab.txt
+cat train_pos.txt train_neg.txt cleared_test_data.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > vocab.txt
 echo "Generating full dict"
-cat train_pos_full.txt train_neg_full.txt test_data.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > vocab_full.txt
+cat train_pos_full.txt train_neg_full.txt cleared_test_data.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > vocab_full.txt
