dce6af0f94bb8b7263ded48c519e3163b4a753b5
Lukas Drescher <lukasd@student.ethz.ch>
2017-06-22 15:47:44 +0200

    lukas config modularized

--- config/lukas_config.py
+++ config/lukas_config.py
@@ -1,14 +1,39 @@
-# Configuration
-positive_tweets='./twitter-datasets/train_pos.txt'
-negative_tweets='./twitter-datasets/train_neg.txt'
+import getpass
+
+azure_config = None
+local_config = None
+
+user_name = getpass.getuser()
+
+if user_name in {"lukasd"}:
+    azure_config = True
+elif user_name in {"lukas"}:
+    local_config = True
+
+if azure_config:
+    # Configuration
+    positive_tweets='./twitter-datasets/train_pos_full.txt'
+    negative_tweets='./twitter-datasets/train_neg_full.txt'
+    vocab_path='./twitter-datasets/vocab_full.txt'
+elif local_config:
+    # Configuration
+    positive_tweets='./twitter-datasets/train_pos.txt'
+    negative_tweets='./twitter-datasets/train_neg.txt'
+    vocab_path='./twitter-datasets/vocab.txt'
+else:
+    raise
+
 test_data='./twitter-datasets/cleared_test_data.txt'
-vocab_path='./twitter-datasets/vocab.txt'
-test_vocab_path='./twitter-datasets/test_vocab.txt'
 remove_unknown_words=True
 
 # Further model parameters of main.py
 word_embedding_dim=400
-validation_split_ratio=0.99
+if azure_config:
+    validation_split_ratio=0.99
+elif local_config:
+    validation_split_ratio = 0.01
+else:
+    raise
 result_file='results/result.csv'
 
 # Further training parameters
@@ -18,7 +43,3 @@ embedding_corpus_name='full.emb'
 # Load model parameters
 model_json = 'model.json'
 model_h5 = 'model.h5'
-
-# Test run parameters
-test_run = True
-test_run_data_ratio=0.01
