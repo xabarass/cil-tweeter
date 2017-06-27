91ca32a7afd6d9cf5c4c72c2eca3f03f78e69759
Lukas Drescher <lukasd@student.ethz.ch>
2017-06-27 18:48:30 +0200

    Removed unnecessary TrainingDataset hierarchy

--- TwitterDataset.py
+++ TwitterDataset.py
@@ -4,6 +4,7 @@ import os
 import random
 import numpy as np
 from tqdm import tqdm
+from keras.preprocessing import sequence
 
 import config
 
@@ -55,12 +56,13 @@ class TwitterDataSet:
     def create_preprocessed_dataset(self, vocabulary, training_validation_split_ratio):
         return PreprocessedDataset(self, vocabulary, training_validation_split_ratio)
 
-    def create_unpreprocessed_dataset(self, training_validation_split_ratio):
-        return UnpreprocessedDataset(self, training_validation_split_ratio)
 
+# TODO: For character embeddings pass in a character vocabulary (mapping tweets to id sequences)
+class PreprocessedDataset:
+    def __init__(self, twitter_dataset, vocabulary, training_validation_split_ratio):
+        assert vocabulary is not None
+        self.vocabulary = vocabulary
 
-class TrainingDataset:
-    def __init__(self, twitter_dataset, training_validation_split_ratio, preprocess_tweet=None):
         self.training_validation_split_ratio = training_validation_split_ratio
 
         self.shuffled_original_train_tweets = []
@@ -77,19 +79,19 @@ class TrainingDataset:
             self.shuffled_original_train_tweets[:], self.shuffled_train_sentiments[:] = zip(*combined_training_dataset)
 
         print("[TrainingDataset] Preprocessing training and test tweets...")
-        if preprocess_tweet is not None:
+        if self.vocabulary is not None:
             self.shuffled_train_tweets = []
             for tweet in tqdm(self.shuffled_original_train_tweets, desc="Training tweets"):
-                self.shuffled_train_tweets.append(preprocess_tweet(tweet))
+                self.shuffled_train_tweets.append(vocabulary.preprocess_and_map_tweet_to_id_seq(tweet))
         else:
             self.shuffled_train_tweets = self.shuffled_original_train_tweets[:]
 
         self.original_test_tweets = twitter_dataset.original_test_tweets
 
-        if preprocess_tweet is not None:
+        if self.vocabulary is not None:
             self.test_tweets = []
             for tweet in tqdm(self.original_test_tweets, desc="Test tweets"):
-                self.test_tweets.append(preprocess_tweet(tweet))
+                self.test_tweets.append(vocabulary.preprocess_and_map_tweet_to_id_seq(tweet))
         else:
             self.test_tweets = self.original_test_tweets[:]
 
@@ -101,33 +103,6 @@ class TrainingDataset:
 
         print("[TrainingDataset] Max tweet length: %d" % self.max_tweet_length)
 
-    def shuffle_and_split(self):
-        """Create training data set"""
-        print("[TrainingDataset] Shuffling data...")
-
-        combined_training_dataset = list(
-            zip(self.shuffled_original_train_tweets, self.shuffled_train_tweets, self.shuffled_train_sentiments))
-        random.shuffle(combined_training_dataset)
-        self.shuffled_original_train_tweets[:], self.shuffled_train_tweets[:], self.shuffled_train_sentiments[:] = zip(
-            *combined_training_dataset)
-
-        nb_validation_samples = int(self.training_validation_split_ratio * len(self.shuffled_train_tweets))
-
-        x_train = self.shuffled_train_tweets[:nb_validation_samples]
-        x_orig_train = self.shuffled_original_train_tweets[:nb_validation_samples]
-        y_train = self.shuffled_train_sentiments[:nb_validation_samples]
-        x_val = self.shuffled_train_tweets[nb_validation_samples:]
-        x_orig_val = self.shuffled_original_train_tweets[nb_validation_samples:]
-        y_val = self.shuffled_train_sentiments[nb_validation_samples:]
-
-        return (x_train, y_train, x_orig_train), (x_val, y_val, x_orig_val)
-
-
-class PreprocessedDataset(TrainingDataset):
-    def __init__(self, twitter_dataset, vocabulary, training_validation_split_ratio):
-        self.vocabulary = vocabulary
-
-        super(PreprocessedDataset,self).__init__(twitter_dataset,training_validation_split_ratio,vocabulary.preprocess_and_map_tweet_to_id_seq)
 
         self.shuffled_preprocessed_train_tweets = []
         for id_seq in tqdm(self.shuffled_train_tweets,desc="Training tweets: generate tokens from id seq"):
@@ -151,7 +126,34 @@ class PreprocessedDataset(TrainingDataset):
         random.shuffle(tokenized_tweets_randomized)
         return tokenized_tweets_randomized
 
-# TODO: Character embeddings need a character vocabulary to map tweets to id sequences
-class UnpreprocessedDataset(TrainingDataset):
-    def __init__(self, twitter_dataset, training_validation_split_ratio):
-        super(UnpreprocessedDataset,self).__init__(twitter_dataset,training_validation_split_ratio)
+    def shuffle_and_split(self):
+        """Create training data set"""
+        print("[TrainingDataset] Shuffling data...")
+
+        combined_training_dataset = list(
+            zip(self.shuffled_original_train_tweets, self.shuffled_train_tweets, self.shuffled_train_sentiments))
+        random.shuffle(combined_training_dataset)
+        self.shuffled_original_train_tweets[:], self.shuffled_train_tweets[:], self.shuffled_train_sentiments[:] = zip(
+            *combined_training_dataset)
+
+        nb_validation_samples = int(self.training_validation_split_ratio * len(self.shuffled_train_tweets))
+
+        x_train = self.shuffled_train_tweets[:nb_validation_samples]
+        x_orig_train = self.shuffled_original_train_tweets[:nb_validation_samples]
+        y_train = self.shuffled_train_sentiments[:nb_validation_samples]
+        x_val = self.shuffled_train_tweets[nb_validation_samples:]
+        x_orig_val = self.shuffled_original_train_tweets[nb_validation_samples:]
+        y_val = self.shuffled_train_sentiments[nb_validation_samples:]
+
+        return (x_train, y_train, x_orig_train), (x_val, y_val, x_orig_val)
+
+
+    def shuffle_and_split_padded(self):
+        (x_train, y_train, x_orig_train), (x_val, y_val, x_orig_val) = self.shuffle_and_split()
+        x_train = sequence.pad_sequences(x_train, maxlen=self.max_tweet_length, value=self.vocabulary.word_to_id['<pad>'] )
+        x_val   = sequence.pad_sequences(x_val,   maxlen=self.max_tweet_length, value=self.vocabulary.word_to_id['<pad>'] )
+        return (x_train, y_train, x_orig_train), (x_val, y_val, x_orig_val)
+
+
+    def test_tweets_padded(self):
+        return sequence.pad_sequences(self.test_tweets, maxlen=self.max_tweet_length, value=self.vocabulary.word_to_id['<pad>'])
