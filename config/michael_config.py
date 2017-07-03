fb28d8ebaa6be99c167d0d8afe0cd76760b2544b
Lukas Drescher <lukasd@student.ethz.ch>
2017-07-03 02:01:45 +0200

    Updated Michis config

--- config/michael_config.py
+++ config/michael_config.py
@@ -1,4 +1,5 @@
 import getpass
+import Models
 
 azure_config = None
 local_config = None
@@ -7,53 +8,97 @@ user_name = getpass.getuser()
 
 if user_name in {"franzm"}:
     azure_config = True
-elif user_name in {}: #TODO: @franzm: fill in your local username here please
+elif user_name in {}:
     local_config = True
-
-if azure_config:
-    # Configuration
-    positive_tweets='./twitter-datasets/train_pos_full.txt'
-    negative_tweets='./twitter-datasets/train_neg_full.txt'
-    vocab_path='./twitter-datasets/vocab_full.txt'
-elif local_config:
-    # Configuration
-    positive_tweets='./twitter-datasets/train_pos.txt'
-    negative_tweets='./twitter-datasets/train_neg.txt'
-    vocab_path='./twitter-datasets/vocab.txt'
-else:
-    raise
-
-test_vocab_path='./twitter-datasets/test_vocab.txt'
-test_data='./twitter-datasets/cleared_test_data.txt'
-remove_unknown_words=True
-
-# Further model parameters of main.py
-word_embedding_dim=400
-if azure_config:
-    validation_split_ratio=0.99
-elif local_config:
-    validation_split_ratio = 0.5
-else:
-    raise
-result_file='results/result.csv'
-misclassified_samples_file = 'misclassified_samples/misclassified_{}_samples'
-
-
-# Further training parameters
-generate_word_embeddings=True
-embedding_corpus_name='full.emb'
-
-# Load model parameters
-model_json = 'model.json'
-model_h5 = 'model.h5'
 
 if azure_config:
     # Test run parameters
     test_run = False
-    test_run_data_ratio=1
 elif local_config:
     # Test run parameters
     test_run = True
-    test_run_data_ratio=0.01
 else:
     raise
+
+# Data set file paths
+
+if azure_config:
+    positive_tweets='./twitter-datasets/train_pos_full.txt'
+    negative_tweets='./twitter-datasets/train_neg_full.txt'
+    vocab_path='./twitter-datasets/vocab_full.txt'
+elif local_config:
+    if not test_run:
+        positive_tweets='./twitter-datasets/train_pos_full.txt'
+        negative_tweets='./twitter-datasets/train_neg_full.txt'
+        vocab_path='./twitter-datasets/vocab_full.txt'
+    else:
+        positive_tweets = './twitter-datasets/train_pos.txt'
+        negative_tweets = './twitter-datasets/train_neg.txt'
+        vocab_path = './twitter-datasets/vocab.txt'
+else:
+    raise
+
+test_vocab_path='./twitter-datasets/test_vocab.txt'
+test_data = './twitter-datasets/cleared_test_data.txt'
+
+# Dataset parameters (size of validation data set)
+if azure_config:
+    validation_split_ratio=0.999
+    test_run_data_ratio=1
+elif local_config:
+    validation_split_ratio = 0.99
+    test_run_data_ratio=0.02
+    test_run_test_data_ratio=0.01
+else:
+    raise
+
+
+### Preprocessing options (token transformation/vocabulary generation)
+vocab_path_opt = { "vocab_path": vocab_path  }
+
+min_word_occurrence = 4
+def final_vocabulary_filter(word, occurrence):
+    return (len(word) > 3 and occurrence > min_word_occurrence) or \
+           (len(word) == 3 and occurrence >= 1.25*min_word_occurrence) or \
+           (len(word) == 2 and occurrence >= 10*min_word_occurrence)  or \
+           (len(word) == 1 and occurrence >=50*min_word_occurrence)
+final_vocabulary_filter.min_word_occurrence = min_word_occurrence # This is used by the Vocabulary and WordEmbedding classes
+
+def preprocessor_vocabulary_filter(word, occurrence):
+    return (len(word) > 3 and occurrence > min_word_occurrence) or \
+           (len(word) == 3 and occurrence >= 3*min_word_occurrence) or \
+           (len(word) == 2 and occurrence >= 100*min_word_occurrence) or \
+           (len(word) == 1 and occurrence >= 1000*min_word_occurrence)
+
+preprocessor_opt = { "remove_unknown_words": True,
+                     "final_vocabulary_filter": final_vocabulary_filter,
+                     "preprocessor_vocabulary_filter": preprocessor_vocabulary_filter}
+
+### ML model options
+
+# Embedding layer parameters
+word_embeddings_opt = {"initializer": "word2vec",
+                       "dim": 400,
+                       "trainable": False,
+                       "corpus_name": None}
+
+# Neural network parameter
+model_builder=Models.SingleLSTM()
+
+ensemble_model_builder=Models.DoubleConv()
+
+# Ensemble parameters
+adaboost_opt = {"algorithm": "SAMME.R",
+                "n_estimators": 15}
+
+# Training parameters
+training_opt = {"epochs":3,
+                "batch_size":64 }
+
+# Results output parameters
+result_file='results/result.csv'
+misclassified_samples_file = 'misclassified_samples/misclassified_{}_samples'
+
+# Load model parameters
+model_save_path = "model"
+
